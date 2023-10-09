1aaf87564b5c31492f9f123da4a808b96d1ce116
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-09 16:35:47 +0300

    Add files via upload

---
+++ indirim-847/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace indirim_847
+{
+    partial class lblFiyat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTutar = new System.Windows.Forms.Label();
+            this.lblİndirim = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.rb10 = new System.Windows.Forms.RadioButton();
+            this.rb15 = new System.Windows.Forms.RadioButton();
+            this.rb25 = new System.Windows.Forms.RadioButton();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTutar
+            // 
+            this.lblTutar.AutoSize = true;
+            this.lblTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTutar.Location = new System.Drawing.Point(59, 78);
+            this.lblTutar.Name = "lblTutar";
+            this.lblTutar.Size = new System.Drawing.Size(45, 24);
+            this.lblTutar.TabIndex = 0;
+            this.lblTutar.Text = "tutar";
+            // 
+            // lblİndirim
+            // 
+            this.lblİndirim.AutoSize = true;
+            this.lblİndirim.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblİndirim.Location = new System.Drawing.Point(59, 128);
+            this.lblİndirim.Name = "lblİndirim";
+            this.lblİndirim.Size = new System.Drawing.Size(115, 24);
+            this.lblİndirim.TabIndex = 1;
+            this.lblİndirim.Text = "indirimli fiyat:";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTutar.Location = new System.Drawing.Point(173, 75);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(130, 29);
+            this.txtTutar.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb5.Location = new System.Drawing.Point(55, 184);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(114, 28);
+            this.rb5.TabIndex = 4;
+            this.rb5.TabStop = true;
+            this.rb5.Text = "%5 indirim";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb10
+            // 
+            this.rb10.AutoSize = true;
+            this.rb10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb10.Location = new System.Drawing.Point(55, 218);
+            this.rb10.Name = "rb10";
+            this.rb10.Size = new System.Drawing.Size(124, 28);
+            this.rb10.TabIndex = 5;
+            this.rb10.TabStop = true;
+            this.rb10.Text = "%10 indirim";
+            this.rb10.UseVisualStyleBackColor = true;
+            // 
+            // rb15
+            // 
+            this.rb15.AutoSize = true;
+            this.rb15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb15.Location = new System.Drawing.Point(55, 252);
+            this.rb15.Name = "rb15";
+            this.rb15.Size = new System.Drawing.Size(124, 28);
+            this.rb15.TabIndex = 6;
+            this.rb15.TabStop = true;
+            this.rb15.Text = "%15 indirim";
+            this.rb15.UseVisualStyleBackColor = true;
+            // 
+            // rb25
+            // 
+            this.rb25.AutoSize = true;
+            this.rb25.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb25.Location = new System.Drawing.Point(55, 286);
+            this.rb25.Name = "rb25";
+            this.rb25.Size = new System.Drawing.Size(124, 28);
+            this.rb25.TabIndex = 7;
+            this.rb25.TabStop = true;
+            this.rb25.Text = "%25 indirim";
+            this.rb25.UseVisualStyleBackColor = true;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(54, 336);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(248, 42);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "____________________";
+            // 
+            // lblFiyat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.rb25);
+            this.Controls.Add(this.rb15);
+            this.Controls.Add(this.rb10);
+            this.Controls.Add(this.rb5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.lblİndirim);
+            this.Controls.Add(this.lblTutar);
+            this.Name = "lblFiyat";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTutar;
+        private System.Windows.Forms.Label lblİndirim;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.RadioButton rb10;
+        private System.Windows.Forms.RadioButton rb15;
+        private System.Windows.Forms.RadioButton rb25;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ indirim-847/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace indirim_847
+{
+    public partial class lblFiyat : Form
+    {
+        public lblFiyat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double tutar = Convert.ToDouble(txtTutar.Text);
+
+            if (rb5.Checked)
+            {
+                tutar = tutar * 0.95; //yüzde 5indirim
+            }
+            else if (rb10.Checked)
+            {
+                tutar = tutar * 0.90;
+            }
+            else if (rb15.Checked)
+            {
+                tutar = tutar * 0.85;
+            }
+            else if (rb25.Checked)
+            {
+                tutar = tutar * 0.75;
+
+                //hesaplama bittikten sonra labal içine ekliyoruz.
+                lblİndirim.Text = tutar.ToString();
+                    
+            }
+        }  
+    }
+}
